ba9082604ffe13e5a2e9677a076a42731e171d69
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-16 21:08:18 -0700

    Added JUnit tests for selection sort in SortingValidator

--- src/test/java/com/example/sortinggame/service/SortingValidatorTest.java
+++ src/test/java/com/example/sortinggame/service/SortingValidatorTest.java
@@ -1,6 +1,8 @@
 package com.example.sortinggame.service;
 
 import com.example.sortinggame.model.GameState;
+import com.example.sortinggame.model.StartRequest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,6 +41,31 @@ class SortingValidatorTest {
                     Collections.swap(array, j, j + 1);
                 }
             }
+        }
+    }
+
+    //Tests first invalid inputs after initialization
+    @Test
+    void testInvalidSelectionSortInitial(){
+        GameState gameState = new GameState(1, "selection_sort", Arrays.asList(5,4,1,3,2));
+        assertFalse(SortingValidator.isValidSwap(gameState,1,0, 2));
+        assertFalse(SortingValidator.isValidSwap(gameState,0,0,1));
+    }
+
+    @Test
+    void testSelectionSortAlgorithm(){
+        List<Integer> array = Arrays.asList(64,25,12,22,11);
+        GameState gameState = new GameState(1,"selection_sort", array);
+        int minIndex;
+        for(int i = 0; i < array.size() - 1; i++){
+            minIndex = i;
+            for(int j = i + 1; j < array.size(); j++){
+                if(array.get(j) < array.get(minIndex)){
+                    minIndex = j;
+                }
+            }
+            assertTrue(SortingValidator.isValidSwap(gameState, i, i, minIndex));
+            Collections.swap(array, i, minIndex);
         }
     }
 }
