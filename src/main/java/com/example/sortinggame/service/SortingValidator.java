ad08d2a4fd73d7806840201b2020decf3f40c374
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-18 22:18:03 -0700

    Added sorting validation method for insertion sort.

--- src/main/java/com/example/sortinggame/service/SortingValidator.java
+++ src/main/java/com/example/sortinggame/service/SortingValidator.java
@@ -4,17 +4,19 @@ import org.springframework.stereotype.Component;
 
 import java.util.*;
 
+//TODO Refactor repeated code in the different swap methods
 @Component
 public class SortingValidator {
 
-    public static boolean isValidSwap(GameState gameState,int pass, int index1, int index2){
+    public static boolean isValidSortParameters(GameState gameState, int pass, int index1, int index2){
+
         switch (gameState.getAlgorithm().toLowerCase()){
             case "bubble_sort":
                 return isValidBubbleSortSwap(gameState,pass, index1, index2);
             case "selection_sort":
                 return isValidSelectionSortSwap(gameState,pass, index1, index2);
-            /*case "insertion_sort":
-                return isValidInsertionSortSwap(array, index1, index2);*/
+            case "insertion_sort":
+                return isValidInsertionSortInsert(gameState, pass, index1, index2);
             //TODO Implement other sorting algorithms later
             default:
                 return false;  // Invalid algorithm
@@ -73,7 +75,7 @@ public class SortingValidator {
 
         int currentPass = gameState.getCurrentPass();
 
-        if(gameState.isFirstSelectionSortSwap() && currentIndex != 0 && currentPass == 0){
+        if(gameState.isFirstSwap() && currentIndex != 0 && currentPass == 0){
             gameState.setInvalidSwap(true);
             gameState.setMessage("Current Index must be 0 during the first pass");
             return false;
@@ -108,11 +110,47 @@ public class SortingValidator {
             gameState.setMessage("Value at Min Index is not the minimum value inside the unsorted portion.");
             return false;
         }
-        gameState.setFirstSelectionSortSwap(false);
+        gameState.setFirstSwap(false);
         gameState.setInvalidPass(false);
         gameState.setInvalidSwap(false);
         gameState.setCurrentPass(currentPass + 1);
         gameState.setMessage("Swap is Valid.");
         return true;
+    }
+
+    private static boolean isValidInsertionSortInsert(GameState gameState, int pass, int sortedIndex, int nextElement){
+        List<Integer> array = gameState.getArray();
+
+        int currentPass = gameState.getCurrentPass();
+        int numInserts = gameState.getNumInserts();
+
+        if(gameState.isFirstSwap() && sortedIndex != 0 && currentPass == 0){
+            gameState.setMessage("Sorted Index must start at 0 on first pass");
+            return false;
+        }
+        if(pass != currentPass){
+            gameState.setInvalidPass(true);
+            gameState.setMessage("Pass is not equal to the current pass.");
+            return false;
+        }
+        if(sortedIndex != numInserts){
+            gameState.setMessage("Sorted Index does not match end of sorted part.");
+            return false;
+        }
+        if(Math.abs(sortedIndex - nextElement) != 1){
+            gameState.setMessage("Next Element must be the next element in the unsorted array.");
+            return false;
+        }
+        if(sortedIndex > nextElement){
+            gameState.setMessage("Next Element index needs to be larger than sorted index.");
+            return false;
+        }
+        gameState.setFirstSwap(false);
+        gameState.setInvalidPass(false);
+        gameState.setCurrentPass(currentPass + 1);
+        gameState.setNumInserts(numInserts + 1);
+        gameState.setMessage("Insertion Valid");
+        return true;
+
     }
 }
