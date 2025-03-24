1930721f9315eb7ce742c11cc9bc01794eecbbb2
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-23 21:08:33 -0700

    Added documentation to key methods.

--- src/main/java/com/example/sortinggame/service/GameService.java
+++ src/main/java/com/example/sortinggame/service/GameService.java
@@ -5,18 +5,23 @@ import com.example.sortinggame.model.ActionRequest;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
+/*Service layer for Spring Boot. Handles start of game, action requests by user, and internal logic */
 @Service
 public class GameService {
-    private final Map<Integer, GameState> games = new HashMap<>();
-    private int gameIdCounter = 1;
+    private final Map<Integer, GameState> games = new HashMap<>(); //Maps a game state to a game id so that each game is unique.
+    private int gameIdCounter = 1; //Keeps track of the current gameID value to associate with a game state
+
+    //Starts the game with a shuffled array for the user to sort. Initializes game state with the current gameID.
     public GameState startGame(StartRequest startRequest){
-        List<Integer> array = generateShuffledArray(startRequest.getArraySize());//TODO Make it so that user can choose size
+        List<Integer> array = generateShuffledArray(startRequest.getArraySize());
         GameState gameState = new GameState(gameIdCounter, startRequest.getAlgorithm(), array);
         games.put(gameIdCounter, gameState);
         return games.get(gameIdCounter++);
     }
 
-    public GameState processSwap(ActionRequest actionRequest){
+    /*Given an actionRequest with several arguments, process the action to validate the action or not according
+    to the selected sorting algorithm.*/
+    public GameState processAction(ActionRequest actionRequest){
         GameState gameState = games.get(actionRequest.getGameID());
         if (gameState == null){
             return null;
@@ -24,31 +29,29 @@ public class GameService {
 
         boolean isValidAction = SortingValidator.isValidSortParameters(gameState, actionRequest.getPass(), actionRequest.getIndex1(), actionRequest.getIndex2());
 
+        //Performs the action requested if it is valid and performs the action associated with the selected algorithm.
         if(isValidAction){
             String algorithm = gameState.getAlgorithm();
 
-            System.out.println("Before action: " + gameState.getArray());
             if(algorithm.equals("bubble_sort") || algorithm.equals("selection_sort")){
                 Collections.swap(gameState.getArray(), actionRequest.getIndex1(), actionRequest.getIndex2());
             }
             else if(algorithm.equals("insertion_sort")){
                 insertIntoSorted(gameState, actionRequest.getIndex2());
             }
-            System.out.println("After action: " + gameState.getArray());
             gameState.incrementSwaps();
         }
-        else{
-            System.out.println("Not a Valid Swap");
-        }
+
+        //Check if the array is sorted before returning data to the user.
         boolean isSorted = isSorted(gameState.getArray());
         if(isSorted){
             gameState.setMessage("List is Sorted. :)");
         }
         gameState.setSorted(isSorted);
-        //return new GameState(gameState.getGameId(), gameState.getAlgorithm(), gameState.getArray());
         return gameState;
     }
 
+    //Creates a shuffled array for the user to sort. Elements consist of integers from 1 - size of array specified by the user
     private List<Integer> generateShuffledArray(int size){
         List<Integer> shuffledArray = new ArrayList<>();
         for(int i = 1; i <= size; i++){
@@ -70,6 +73,7 @@ public class GameService {
         array.set(j + 1, key);
     }
 
+    //Checks if the array is in ascending sorted order.
     private boolean isSorted(List<Integer> arr){
         for(int i = 0; i < arr.size() - 1; i++){
             if(arr.get(i) > arr.get(i+1)){
@@ -79,23 +83,10 @@ public class GameService {
         return true;
     }
 
+    //Method for the front-end to receive the current game state.
     public GameState getGameState(int gameId){
         GameState gameState = games.get(gameId);
         System.out.println(gameId + " is the gameID and array is" + gameState.getArray());
         return gameState;
     }
-
-    /*private void printList(List<Integer> list){
-        for(int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i) + ",");
-        }
-    }*/
-
-    /*public static void main(String[] args){
-        int k = 5;
-        GameService obj = new GameService();
-
-        obj.printList(obj.generateRandomArray(k));
-    }*/
-
 }
