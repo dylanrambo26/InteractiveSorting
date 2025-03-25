a20572fe557a8067a5f9b70cd238d493b04e5c42
dylanrambo26 <DYLANRAMBO.26@GMAIL.COM>
2025-03-25 11:24:47 -0700

    Deleted misplaced src folder from projec root.

--- src/main/java/com/example/sortinggame/repository/GameStateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sortinggame.repository;
-import com.example.sortinggame.model.GameState;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameStateRepository extends JpaRepository<GameState, Integer> {
-}
